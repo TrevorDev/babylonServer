b85f26f1b3b712a2173c71f22863cb54a3ea68dd
Trevor Baron <trbaron@microsoft.com>
2019-01-25 10:57:23 -0800

    more complex scene

--- apps/testApp.ts
+++ apps/testApp.ts
@@ -32,15 +32,33 @@ window.addEventListener("resize", ()=> {
     engine.resize()
 })
 
-var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene)
-camera.setTarget(BABYLON.Vector3.Zero())
-camera.attachControl(canvas, true)
-var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene)
-light.intensity = 0.7
+// var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene)
+// camera.setTarget(BABYLON.Vector3.Zero())
+// camera.attachControl(canvas, true)
+// var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene)
+// light.intensity = 0.7
 
 
-var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene)
-sphere.position.y = 1
+// var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene)
+// sphere.position.y = 1
 
-var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 1, scene)
-ground.material = new BABYLON.StandardMaterial("",scene)
+// var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 1, scene)
+// ground.material = new BABYLON.StandardMaterial("",scene)
+
+
+var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1, 0.8, 5, new BABYLON.Vector3(0, 0, 0), scene);
+    
+camera.attachControl(canvas, true);
+
+camera.lowerRadiusLimit = 2.5;
+camera.upperRadiusLimit = 10;
+camera.pinchDeltaPercentage = 0.01;
+camera.wheelDeltaPercentage = 0.01;
+
+scene.clearColor = new BABYLON.Color3(0.0, 0.0, 0.0);
+
+// Lets create a sun effect made of 3 different particle system
+// Definition: https://assets.babylonjs.com/particles/systems/sun.json
+BABYLON.ParticleHelper.CreateAsync("sun", scene).then((set) => {
+    set.start();
+});
